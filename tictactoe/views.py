51ca5f214f921e4a26fc27e2a2568f3bfae50234
Ethan Glasser-Camp <ethan@betacantrips.com>
2013-12-24 15:50:20 -0500

    simple template for index view

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from tictactoe.models import TicTacToeGame
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tictactoe index.")
+    latest_game_list = TicTacToeGame.objects.order_by('-updated')[:5]
+    return render(request, 'tictactoe/index.html', {
+        'latest_game_list': latest_game_list,
+    })
 
 def detail(request, game_id):
     return HttpResponse("You're looking at game {}.".format(game_id))
