a833e42a199c14c409bf89a600b04f9a81633b3b
Ethan Glasser-Camp <ethan@betacantrips.com>
2013-12-24 16:51:44 -0500

    stop using hardcoded URLs

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -5,6 +5,6 @@ from tictactoe import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<game_id>\d+)/$', views.detail, name='index'),
-    url(r'^(?P<game_id>\d+)/move/$', views.move, name='index'),
+    url(r'^(?P<game_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<game_id>\d+)/move/$', views.move, name='move'),
 )
